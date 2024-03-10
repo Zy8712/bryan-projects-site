63a7319a080c6d047aeef66ba8d677841d5ad491
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-03-10 07:10:49 +0000

    Fixed the icon rendering issues.
    Specifics:
    - issue had to do with the way I was passing the values from the json file as a prop to the PrimaryIcons component

--- src/components/upcoming-page-components/ProjectPrimaryIcons.jsx
+++ src/components/upcoming-page-components/ProjectPrimaryIcons.jsx
@@ -48,8 +48,11 @@ function ProjectPrimaryIcons(props) {
                     src={icon.iconImage}
                     alt={icon.alt}
                     title={icon.title}
-                    className={`w-[30px] ${(activeIconCount - 1) > 7 ? 'mx-[6px]' : 'mx-2'} ${props.technologies[icon.trueFalse] ? 'inline' : 'hidden'}`}
+                    className={`w-[30px] ${(activeIconCount - 1) > 7 ? 'mx-[6px]' : 'mx-2'} ${props[icon.trueFalse] ? 'inline' : 'hidden'}`}
                 />
+                <div>
+                    {props.html_TF}
+                </div>
                 {/** Subtract 1 from activeIconCount to take into account one of the boolean variables is 
                  *  frontendMentorTF, which isn't an icon and is just accidentially counted in the process above.
                  */}
--- src/components/upcoming-page-components/RenderUpcoming.jsx
+++ src/components/upcoming-page-components/RenderUpcoming.jsx
@@ -1,34 +1,30 @@
 import upcoming_data from "./upcoming-projects.json";
 import Info_Icon from '../../assets/info-svgrepo-com.svg';
 import FrontendMentor from '../../assets/frontend-mentor-logo-transparent.png';
+import ProjectPrimaryIcons from "./ProjectPrimaryIcons";
 
 export default function RenderUpcoming() {
-    
     return (
         <>
             {upcoming_data.map((item, index) => (
                 <div className="group box w-[360px] custom-sm:w-[400px] h-[510px] custom-sm:h-[505px] overflow-hidden bg-white rounded-2xl 
-                      border-none mb-8 cursor-pointer relative hover:shadow-2xl hover:shadow-white	">
+                      border-none mb-8 cursor-pointer relative hover:shadow-2xl hover:shadow-white	" key={index}>
 
                     <div className="w-full h-[252.3px] custom-sm:h-[293.333333px] rounded-2xl overflow-hidden relative">
-
-                        {/**<ProjectPreview previewImage={props.previewImage} />**/}
-                        <img src={`${item.preview_image}`}
+                        <img
+                            src={item.preview_image}
                             alt="qr_code_preview"
                             className="w-full rounded-2xl duration-300 transform scale-100 group-hover:scale-108 transition-transform"
                         />
-
                     </div>
 
                     <div className={`h-[257.7px] custom-sm:h-[211.67px] text-white flex flex-col relative bg-gradient-to-b from-light-gray to-very-dark-blue`}>
-
                         <div className="mx-11 flex flex-col justify-between items-center text-center relative">
                             <span className="block mt-4 mb-3 text-2xl font-medium">{item.name}</span>
                             <p className="mb-3">{item.date}</p>
-
                             <div className="flex justify-center items-center">
+                                <ProjectPrimaryIcons {...item.technologies} />
                             </div>
-
                             <div className={`w-[30px] ${item.frontend_mentor_project ? 'flex' : 'hidden'} flex-col absolute top-4 -right-9 
                     bg-white border-2 border-solid border-transparent rounded-full hover:bg-nav-gradient-1`}>
                                 <a href={item.links.challenge_link} target="_blank">
@@ -36,16 +32,7 @@ export default function RenderUpcoming() {
                                     <img src={FrontendMentor} alt="frontendmentor_icon" />
                                 </a>
                             </div>
-
                         </div>
-
-                        {/**<ProjectDetails {...props}>
-              
-                          <ProjectPrimaryIcons {...props} />
-              
-            </ProjectDetails>**/}
-
-                        {/**<ProjectWorkLinks githubrepo={props.githubrepo} websitepreview={props.websitepreview} />**/}
                         <div className="w-full h-[50px] flex absolute bottom-0">
                             <div className="w-1/2 h-full flex justify-start items-center ml-11">
                                 <a className="link-custom" href={item.links.github_link} target="_blank">
@@ -61,7 +48,6 @@ export default function RenderUpcoming() {
                             </div>
                         </div>
                     </div>
-
                 </div>
             ))}
         </>
