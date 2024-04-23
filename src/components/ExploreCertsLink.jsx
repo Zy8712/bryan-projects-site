2737d54858bdfa3cf334f9600fb066f5940f3e4f
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-04-23 06:13:52 +0000

    Adjusted spacing.

--- src/components/ExploreCertsLink.jsx
+++ src/components/ExploreCertsLink.jsx
@@ -2,7 +2,7 @@
 export default function ExploreCertsLink() {
     return (
         <>
-            <div className="w-28 absolute bottom-7 left-2 hidden custom-xl:block">
+            <div className="w-28 absolute bottom-8 left-2 hidden custom-xl:block">
                 <a href="https://bryan-certificates-site.vercel.app/" target="_blank"
                     className="relative group w-full h-full flex flex-col items-center text-center">
                     <img src="/space-svgrepo-com.svg" alt="" className="w-24" />
@@ -11,7 +11,7 @@ export default function ExploreCertsLink() {
                     </span>
                     <img src="/external-link-svgrepo-com.svg" alt="" className="absolute top-4 right-0 z-30 w-5 h-5" />
 
-                    <div className="absolute top-2 -z-30 w-[126px] h-28 rounded-md group-hover:bg-black group-hover:bg-opacity-15"></div>
+                    <div className="absolute top-2 -z-30 w-[126px] h-[116px] rounded-md group-hover:bg-black group-hover:bg-opacity-15"></div>
                 </a>
             </div>
         </>
--- src/components/ExploreProfileSiteLink.jsx
+++ src/components/ExploreProfileSiteLink.jsx
@@ -2,7 +2,7 @@
 export default function ExploreProfileSite() {
     return (
         <>
-            <div className="w-28 absolute bottom-9 right-0 hidden custom-xl:block">
+            <div className="w-32 absolute bottom-10 right-1 hidden custom-xl:block">
                 <a href="https://bryanli.vercel.app/" target="_blank"
                     className="relative group w-full h-full flex flex-col items-center text-center">
                     <img src="/rocket-ship-svgrepo-com.svg" alt="" className="w-20" />
@@ -11,7 +11,7 @@ export default function ExploreProfileSite() {
                     </span>
                     <img src="/external-link-svgrepo-com.svg" alt="" className="absolute top-0 right-1 z-30 w-5 h-5" />
 
-                    <div className="absolute -top-3 -z-30 w-28 h-32 rounded-md group-hover:bg-black group-hover:bg-opacity-15"></div>
+                    <div className="absolute -top-3 -z-30 w-32 h-32 rounded-md group-hover:bg-black group-hover:bg-opacity-15"></div>
                 </a>
             </div>
         </>
